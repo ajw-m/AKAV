106741570d8f8bf285defd3f7b0280fd25fafd57
ajw@monark.com <!((?signon123!>
2021-03-30 15:19:29 -0700

    Using typescript

---
+++ AKAV.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import {
+	ScrollView,
+	View,
+	Keyboard,
+	KeyboardAvoidingView,
+	Platform,
+	SafeAreaView,
+} from 'react-native';
+import styled from 'styled-components';
+
+const MainView = styled(SafeAreaView)`
+	background: ${(props) => (props.background ? props.background : '#FFFFFF')};
+	flex: 1;
+`;
+
+const Box = styled(View)`
+	padding-left: ${(props) => props.pl}px;
+	padding-right: ${(props) => props.pr}px;
+	flex: 1;
+	justify-content: flex-end;
+`;
+
+interface AKAVProps {
+	children: [React.ReactNode];
+	background: string;
+	pl: number;
+	pr: number;
+	needsSTB: false | true;
+	submitButton: React.ReactNode;
+	offset: number;
+	testID: string;
+}
+
+const AKAV = ({
+	children,
+	background = '#FFF',
+	pl = 20,
+	pr = 20,
+	needsSTB = false,
+	submitButton,
+	offset = 100,
+	testID = '',
+}: AKAVProps) => {
+	let sv = React.useRef(null);
+
+	const _keyboardDidShow = () => {
+		if (!sv || !sv.current) return;
+		if (!needsSTB) return;
+		sv.current.scrollToEnd();
+		console.warn(sv);
+	};
+
+	const _keyboardDidHide = () => {
+		console.warn('Keyboard Hidden');
+	};
+
+	React.useEffect(() => {
+		Keyboard.addListener('keyboardWillShow', _keyboardDidShow);
+		Keyboard.addListener('keyboardDidHide', _keyboardDidHide);
+
+		// cleanup function
+		return () => {
+			Keyboard.removeListener('keyboardWillShow', _keyboardDidShow);
+			Keyboard.removeListener('keyboardDidHide', _keyboardDidHide);
+		};
+	}, []);
+
+	return (
+		<KeyboardAvoidingView
+			style={{
+				flex: 1,
+				backgroundColor: background,
+			}}
+			keyboardVerticalOffset={offset}
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+		>
+			<MainView testID={testID} background={background}>
+				<Box pl={pl} pr={pr}>
+					<ScrollView
+						showsVerticalScrollIndicator={false}
+						showsHorizontalScrollIndicator={false}
+						keyboardShouldPersistTaps={'handled'}
+						ref={sv}
+						style={{ height: '100%' }}
+					>
+						{children}
+					</ScrollView>
+					{submitButton}
+				</Box>
+			</MainView>
+		</KeyboardAvoidingView>
+	);
+};
+
+export default AKAV;
